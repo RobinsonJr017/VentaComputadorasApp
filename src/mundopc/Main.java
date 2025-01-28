a9e1e065922f587af2ec7eb195d8d857efdd9c68
Robinson Lopez <robins.616009@gmail.com>
2025-01-28 13:49:19 -0500

    Clase monitor

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,4 +1,16 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 public class Main {
+    public static void main(String[] args) {
+        Raton ratonLenovo = new Raton("blutooth", "lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoRedragon = new Teclado("tipo C", "redragon");
+        System.out.println(tecladoRedragon);
+        Monitor monitorLenovo = new Monitor("Lenovo", 27);
+        System.out.println(monitorLenovo);
+    }
 }
